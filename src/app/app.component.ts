9432cab8ce427ccdee4e2f82f81eb1385b878e44
Leo Loureiro <leolou.90@gmail.com>
2025-06-07 21:56:58 -0700

    add components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GeneratorComponent } from "../components/generator/generator.component";
+import { PasswordFieldComponent } from "../components/password-field/password-field.component";
+import { CheckboxComponent } from "../components/checkbox/checkbox.component";
+import { SliderComponent } from "../components/slider/slider.component";
+import { StrenghtStateComponent } from "../components/strenght-state/strenght-state.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GeneratorComponent, PasswordFieldComponent, CheckboxComponent, SliderComponent, StrenghtStateComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
