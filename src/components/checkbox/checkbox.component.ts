e879a9bb36de9b58a3fbb44c9dee0b32e260dccc
Leo Loureiro <leolou.90@gmail.com>
2025-06-22 19:33:53 -0700

    correct the styles

--- src/components/checkbox/checkbox.component.ts
+++ src/components/checkbox/checkbox.component.ts
@@ -4,6 +4,7 @@ import { GeneratorService } from '../../services/generator/generator.service';
 @Component({
   selector: 'app-checkbox',
   templateUrl: './checkbox.component.html',
+  styleUrls: ['./checkbox.component.scss'],
   standalone: true,
 })
 export class CheckboxComponent {
@@ -13,25 +14,21 @@ export class CheckboxComponent {
 onUpperChange(event: Event): void {
   const input = event.target as HTMLInputElement;
   this.generatorService.SetUpperCase = input.checked;
-  console.log(this.generatorService.GetUpperCase);
 }
 
 onLowerChange(event: Event): void {
   const input = event.target as HTMLInputElement;
   this.generatorService.SetLowerCase = input.checked;
-  console.log(this.generatorService.GetLowerCase);
 }
 
 onNumbersChange(event: Event): void {
   const input = event.target as HTMLInputElement;
   this.generatorService.SetNumbers = input.checked;
-  console.log(this.generatorService.GetNumbers);
 }
 
 onSymbolsChange(event: Event): void {
   const input = event.target as HTMLInputElement;
   this.generatorService.SetSymbols = input.checked;
-  console.log(this.generatorService.GetSymbols);
 }
 
 }
