fbe8eabbec163197d7bd08afecd51718f0911dab
Leo Loureiro <leolou.90@gmail.com>
2025-06-22 19:34:58 -0700

    add method to function

--- src/components/generator/generator.component.ts
+++ src/components/generator/generator.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { PasswordFieldComponent } from '../password-field/password-field.component';
+import { CommonModule } from '@angular/common';
 import { CheckboxComponent } from '../checkbox/checkbox.component';
 import { SliderComponent } from '../slider/slider.component';
 import { StrenghtStateComponent } from '../strenght-state/strenght-state.component';
+import { GeneratorService } from '../../services/generator/generator.service';
 
 @Component({
   selector: 'app-generator',
@@ -13,4 +15,15 @@ import { StrenghtStateComponent } from '../strenght-state/strenght-state.compone
 })
 export class GeneratorComponent {
 
+  @ViewChild(PasswordFieldComponent)
+  passwordFieldComponent!: PasswordFieldComponent;
+
+  constructor(public generatorService: GeneratorService){
+
+  }
+
+  generate() {
+    this.generatorService.generatePassword();
+    console.log(this.generatorService.generatedPassword)
+  }
 }
