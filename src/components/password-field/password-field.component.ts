460dcce4e88701042badc25ef36ab5ca4245253b
Leo Loureiro <leolou.90@gmail.com>
2025-06-22 19:35:52 -0700

    add variable async to show password

--- src/components/password-field/password-field.component.ts
+++ src/components/password-field/password-field.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { GeneratorService } from '../../services/generator/generator.service';
+import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-password-field',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './password-field.component.html',
   styleUrl: './password-field.component.scss'
 })
 export class PasswordFieldComponent {
 
+
+  generatedPassword: string = '';
+  private sub!: Subscription;
+
+  constructor(public generatorService: GeneratorService) {}
+
+  ngOnInit(): void {
+    this.sub = this.generatorService.password$.subscribe(password => {
+      this.generatedPassword = password;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 }
