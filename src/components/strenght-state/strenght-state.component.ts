b3fc570b2a2e15c589721ac018ff59c8699be59c
Leo Loureiro <leolou.90@gmail.com>
2025-06-23 19:11:08 -0700

    add strenght division and colors filling according password

--- src/components/strenght-state/strenght-state.component.ts
+++ src/components/strenght-state/strenght-state.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { GeneratorService } from '../../services/generator/generator.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-strenght-state',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './strenght-state.component.html',
   styleUrl: './strenght-state.component.scss'
 })
 export class StrenghtStateComponent {
+  constructor(public generatorService: GeneratorService) {}
 
+  get strength(): number {
+    return this.generatorService.getStrengthLevel();
+  }
+
+  getBoxColor(position: number): string {
+    if (this.strength >= position) {
+      if (this.strength === 1) return 'box-red';
+      if (this.strength === 4) return 'box-green';
+      return 'box-yellow';
+    }
+    return 'box-empty';
+  }
 }
