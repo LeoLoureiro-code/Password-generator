774036d53860a1cd242f9ed4e43b2ef114c62a25
Leo Loureiro <leolou.90@gmail.com>
2025-06-24 18:49:45 -0700

    add copy to clickboard functionality

--- src/components/password-field/password-field.component.ts
+++ src/components/password-field/password-field.component.ts
@@ -3,7 +3,6 @@ import { GeneratorService } from '../../services/generator/generator.service';
 import { Subscription } from 'rxjs';
 import { CommonModule } from '@angular/common';
 
-
 @Component({
   selector: 'app-password-field',
   standalone: true,
@@ -12,9 +11,9 @@ import { CommonModule } from '@angular/common';
   styleUrl: './password-field.component.scss'
 })
 export class PasswordFieldComponent {
-
-
   generatedPassword: string = '';
+  copied: boolean = false;
+
   private sub!: Subscription;
 
   constructor(public generatorService: GeneratorService) {}
@@ -27,5 +26,16 @@ export class PasswordFieldComponent {
 
   ngOnDestroy(): void {
     this.sub.unsubscribe();
+  }
+
+  copyToClipboard(): void {
+    if (!this.generatedPassword) return;
+
+    navigator.clipboard.writeText(this.generatedPassword).then(() => {
+      this.copied = true;
+      setTimeout(() => (this.copied = false), 2000);
+    }).catch(err => {
+      console.error('Error al copiar la contraseña:', err);
+    });
   }
 }
